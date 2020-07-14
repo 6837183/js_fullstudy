f160a411d1b918142629ebda5ec616c7add5a88c
6837183 <2501520585@qq.com>
2020-07-14 18:53:37 +0800

    小鸟飞翔

--- book/the_good_parts/chapter4/san.js
+++
@@ -1,9 +0,0 @@
-if(typeof Object.beget !== 'function'){
-    Object.beget=function(0){
-        var F =function(){}
-            F.prototype=0;
-            return new F();
-        };
-    
-}
-var another_stooge=Object.beget(stooge);
---
+++ css/qunar/readme.md
@@ -0,0 +1,6 @@
+- 页面思维工程
+  1. DIV + CSS 勇闯天涯
+    盒子模型
+  2. JS交互
+     站点互换 DOM
+  3. float position 布局
