e9e9d3e45ea1fa8bb0baf7b61448dc05f1eb2fe1
6837183 <2501520585@qq.com>
2020-07-15 20:00:34 +0800

    wang

--- talk_in_js/cherrio/1.js
+++ talk_in_js/cherrio/1.js
@@ -1,46 +1,13 @@
-<!DOCTYPE html>
-<html>
- <head>
-  <title> 事件</title>  
-  <script type="text/javascript">
-   function count(){
-       var d = "";
-    var a=document.getElementById("txt1").value;   
-    //获取第一个输入框的值
-    var b=document.getElementById("txt2").value;
-	//获取第二个输入框的值
-    var c=document.getElementById("select").value;
-	//获取选择框的值
-    switch(c)
-	{
-     case "+":
-     //d = a + b;
-     d = parseInt(a)+parseInt(b);
-     break;
-     case "-":
-     d = a-b;
-     break;
-     case "*":
-     d = a*b;
-     break;
-     default:
-     d = a/b;
-     }
-    document.getElementById("fruit").value   = d;
-    
-   }
-  </script> 
- </head> 
- <body>
-   计数器：<input type='text' id='txt1' /> 
-   <select id='select'>
-		<option value='+'>+</option>
-		<option value="-">-</option>
-		<option value="*">*</option>
-		<option value="/">/</option>
-   </select>
-   <input type='text' id='txt2' /> 
-   <input type='button' value='=' onclick="count()"/> <!--通过 = 按钮来调用创建的函数，得到结果--> 
-   <input type='text' id='fruit' />   
- </body>
-</html>
+<view class="wrapper">
+  <view class="group">
+    <view class="group-bar">
+      <view class="group-title">7月15日</view>
+    </view>
+    <view class="group-content">
+      <view class="group-content-item">
+        <view class="group-content-item-desc">跟王宝强学英语</view>
+        <image mode="aspectFill" class="group-content-item-img" src="https://puui.qpic.cn/qqvideo_ori/0/q0033kobh9p_228_128/0"/>
+      </view>
+    </view>
+  </view>
+</view>
