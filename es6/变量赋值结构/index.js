b987afb8ce56a4ed9222263db6b0f59cc09bacd0
6837183 <2501520585@qq.com>
2020-07-21 19:26:53 +0800

    blfzjg

--- es6/变量赋值结构/index.js
+++ es6/变量赋值结构/index.js
@@ -25,6 +25,32 @@
 
 // let [x,y = 'aaa'] = ['a',null]
 
-let [x=1,y=x]= []
-console.log(x,y)//underfined解构出来的
+// let [x=1,y=x]= [2,3]
+// console.log(x,y)//underfined解构出来的
 
+// let {foo,bar} = { foo: 'aaa',bar:'bbb'}
+// console.log(foo,bar)
+
+// let {foo:baz} = {foo:'aaa',bar:'bbb'}
+// //foo 是匹配模式，baz才是变量
+// console.log (baz)
+
+// let obj = {
+//     name: '小计',
+//     desc: {
+//         alone:true,
+//         age:21,
+//         sanwei:[50,100,120]
+//     }
+// }
+// let{name,desc:{alone,age},desc:{sanwei:[x,y,z]}}=obj
+// console.log(name,alone,age,x,y,z)
+
+
+//字符串的解构
+let [a,b,c,d,e] = 'hello'
+let {length: len} = 'hello'
+console.log(len)
+
+// //数值和布尔值的解构赋值
+// let {}
