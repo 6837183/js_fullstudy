716afb1e680cbd6cfe49273cd89bd7d93946f967
6837183 <2501520585@qq.com>
2020-07-24 12:08:56 +0800

    a

--- es6/变量赋值结构/index.js
+++ es6/变量赋值结构/index.js
@@ -1,56 +1,6 @@
-// let a = 1
-// let b = 2
-// let c = 3
-// console.log(a)
-
-// // let [a,b,c] = [1,2,3]
-
-// let [foo,[[bar],baz]] = [1,[[2],3]]
-// console.log(foo)
-// console.log(bar)
-// console.log(baz)
-// let [, , third] = ['a','b','c']
-// console.log(third)
-// let [x,y] = [1,2,3]
-// console.log(x,y)
-
-// let [head,...tail] = [1,2,3,4]
-// console.log(head,tail)
-
-// let [a,b,...c] = ['hello']
-// console.log(a,b,c)
-
-// let [foo] = {}
-// console.log(foo)
-
-// let [x,y = 'aaa'] = ['a',null]
-
-// let [x=1,y=x]= [2,3]
-// console.log(x,y)//underfined解构出来的
-
-// let {foo,bar} = { foo: 'aaa',bar:'bbb'}
-// console.log(foo,bar)
-
-// let {foo:baz} = {foo:'aaa',bar:'bbb'}
-// //foo 是匹配模式，baz才是变量
-// console.log (baz)
-
-// let obj = {
-//     name: '小计',
-//     desc: {
-//         alone:true,
-//         age:21,
-//         sanwei:[50,100,120]
-//     }
-// }
-// let{name,desc:{alone,age},desc:{sanwei:[x,y,z]}}=obj
-// console.log(name,alone,age,x,y,z)
-
-
-//字符串的解构
-let [a,b,c,d,e] = 'hello'
-let {length: len} = 'hello'
-console.log(len)
-
-// //数值和布尔值的解构赋值
-// let {}
+let arr = [2,3,4,1,3,2,5]
+console.log(arr.indexOf(4))
+console.log([...new Set(arr)])
+console.log([...new Set('abcdab')])
+arr.splice(3,1)
+console.log(arr)
