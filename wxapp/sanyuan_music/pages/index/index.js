018dedd7b09e016a64da1d098892ad57428199dc
6837183 <2501520585@qq.com>
2020-07-29 16:22:46 +0800

    q

---
+++ wxapp/sanyuan_music/pages/index/index.js
@@ -0,0 +1,36 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    bannerList:[],
+    recommendList:[],
+    isRecommendListLoading:true
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    wx.request({
+      url:'',
+      success(){
+        this.setData({
+          isRecommendListLoading:false
+        })
+      }
+    })
+    
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
