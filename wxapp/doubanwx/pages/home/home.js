44ed724300b2242987777844b491a296bd1d3ee6
6837183 <2501520585@qq.com>
2020-07-29 14:35:26 +0800

    e

--- wxapp/doubanwx/pages/home/home.js
+++ wxapp/doubanwx/pages/home/home.js
@@ -12,7 +12,52 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+      this.getCity((city) =>{
+        console.log(city)
+        this.loadData(0,{city:city, apikey:'0df993c66c0c636e29ecbb5344252a4a'})
+      })
+  },
+  loadData(idx, params) {
+    console.log(params)
+    let url = wx.db.url('/v2/movie/in_theaters')
+    console.log(url)
+    wx.request({
+      url: url,
+      data: params,
+      header: {'content-type': 'json'},
+      success: (res) => {
+        console.log(res)
+      },
+      fail:()=>{console.log('获取失败')}
+    })
+  },
+  getCity( succeed){
+    // 拿到当前所在的城市名称
+    // 先拿到当前所在区域的经纬度(小程序自带api)
+    wx.getLocation({
+      type: 'wgs84', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
+      success: (res) => {
+        // success
+        // console.log(res)
+        //将经纬度装换成具体的名称
+        wx.request({
+          url: 'https://api.map.baidu.com/reverse_geocoding/v3',
+          data: {
+            ak: 'AgstQBQMuypsnELAm0YdNXfSjdHC9Xld',
+            output: 'json',
+            coordtype: 'wgs84ll',
+            location: `${res.latitude},${res.longitude}`
+          },
+          success: (res) => {
+            console.log(res)
+            let city = res.data.result.addressComponent.city
+            succeed &&  succeed(city)
+          },
+          fail:()=>{console.log('获取城市失败')}
+        })
+      },
+      fail: () => {}
+    });
   },
 
   /**
