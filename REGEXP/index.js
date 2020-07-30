1e81df2f4b8d258c30ad1df451f6ec20d8b38c2b
6837183 <2501520585@qq.com>
2020-07-30 16:53:07 +0800

    a

--- REGEXP/index.js
+++ REGEXP/index.js
@@ -24,5 +24,45 @@ let patt2 = /word/
 // console.log(pat2.test(str))
 
 //字符串的正则
-let str = 'Visit W3SChool'
-console.log(str.search(/w3school/i))
+// let str = 'Visit W3SChool'
+// console.log(str.search(/w3school/i))
+
+// let str = '1 plus 2 equl 33'
+// console.log(str.match(/\d+/g))
+
+// let str = "Hello pp! oh I am wn"
+// console.log(str)
+// console.log(str.replace(/wn/,"snail"))
+
+// console.log(str.split(/\s+/))
+
+//正则写法
+// [abc]查找方括号之间的任何字符
+// let str = "Is this all there is?"
+// let pat = /[a-z]/gi
+// console.log(str.match(pat))
+// [^abc]查找任何不在方括号之间的字符
+// let str = "hello pp like jinlong"
+// let pat = /[^like]/g
+// console.log(str.match(pat))
+
+// let str = 'hello siming! How are you?'
+// let pat = /hello|you/g
+// console.log(str.match(pat))
+
+// let str = "That's hot! 100%"
+// let pat = /\w/g
+// console.log(str.match(pat))
+
+// let str = 'moon'
+// let pat = /oon\b/g
+// console.log(str.match(pat))
+
+// let str = 'Is this his'
+// let pat = /is$/g
+// console.log(str.match(pat))
+
+// 手机号正则
+    let tel = '18370397660'
+    let par = /^1([3-9])\d{9}/
+    console.log(par.test(tel))
