7fe64fc80e7a45ec3ddc8940be74a3c6706666d2
6837183 <2501520585@qq.com>
2020-08-02 21:14:41 +0800

    a

--- book/1.js
+++ book/1.js
@@ -1,20 +1,76 @@
-function calculateBonus(salary,level){
-    console.log(arguments);
-    if ( arguments.length < 2) {
-        throw new Error('传参不正确，请检查传递工资及等级')
-    }
+// component/ratingStar/ratingStar.js
+const STAR_ON = "../../assets/imgs/2.png";
+const STAR_OFF= "../../assets/imgs/1.png";
+const STAR_HALF = "../../assets/imgs/3.png";
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+      score: {
+        type: Number,
+       observer:function(newVal,oldVal,path){
+        let stars = [STAR_OFF,STAR_OFF,STAR_OFF,STAR_OFF,STAR_OFF]
+        if(newVal>0){
+          newVal = newVal / 10
+          let floor = Math.floor(newVal)
+          if(floor != newVal){ //说明有小数
+            stars[floor] = STAR_HALF
+          }
+          for(let i = 0; i<floor; i++){
+            stars[i] = STAR_ON
+          }
+        }
+        this.setData({
+          stars:stars
+        })
+       }
+      },
+      iconSize:{
+        type:String,
+        value:'26rpx'
+      }
+  },
 
-    if (typeof salary != 'number' || salary < 0){
-        throw new TypeError('工资必须是整数');
-        salary = parseInt(salary)
-    }
-    if ( level == 'C'){
-        return salary * 16;
-    } else if( level == 'B'){
-        return salary * 18
-    } else if( level == 'A') {
-        return salary * 20
-    }
-    return salary * 12
-}
-console.log(calculateBonus('20000','A'));
+  /**
+   * 组件的初始数据
+   */
+  data: {
+      // list:[
+      // ]
+      stars: [STAR_OFF, STAR_OFF, STAR_OFF, STAR_OFF, STAR_OFF]
+
+  },
+ lifetimes: {
+    // attached: function() {
+    //     let score = this.data.score
+    //     let list = this.data.list
+    //     let scoreint = parseInt(score)
+    //     for (let i=0;i<scoreint;i++){
+    //       list.push('../../assets/imgs/2.png')
+    //     }
+    //     if(score==scoreint){
+    //       for( let j=0;j<5-scoreint;j++)
+    //         list.push('../../assets/imgs/1.png')
+    //     }else{
+          
+    //        list.push('../../assets/imgs/3.png')
+    //       for(let j=0;j<5-1-score;j++){
+    //         list.push('../../assets/imgs/1.png')
+    //       }
+        
+    //     }
+    //     this.setData({
+    //       list : list
+    //     })
+
+        
+    // }
+ },
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
