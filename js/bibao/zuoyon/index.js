af640d41ca0597ce3aa18dff4731eb5cf177b9ed
6837183 <2501520585@qq.com>
2020-08-11 10:58:17 +0800

    a

---
+++ js/bibao/zuoyon/index.js
@@ -0,0 +1,19 @@
+function test(){
+    //
+}
+
+
+//函数也能拥有属性
+// test.name test.prototype
+// test.[[scope]]  //隐式属性
+
+// test()  ---> AO: {} //回收
+// test()  ---> AO: {}
+
+function a(){
+    function b(){
+        var b = 222
+    }
+    var a = 111
+    
+}
