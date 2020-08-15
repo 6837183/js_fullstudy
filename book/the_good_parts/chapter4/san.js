b7b21a69fac33af775ef3620dd0e1b8385ea0fb3
6837183 <2501520585@qq.com>
2020-08-15 18:06:32 +0800

    a

--- book/the_good_parts/chapter4/san.js
+++ book/the_good_parts/chapter4/san.js
@@ -1,23 +1,15 @@
-var duck = {
-    duckSing:function(){
-        console.log("嘎嘎嘎");
-    }
-}
-var cherrio = [];
-console.log(typeof cherrio);
+//logs.js
+const util = require('../../utils/util.js')
 
-for (var i=0; i<1000; i++)
-{
-    cherrio.push(duck)
-}
-if(cherrio.length === 1000){
-    console.log('已招募完成，报告国王');
-}
-duck.duckSing();
- for( let singer of cherrio){
-    //  cherrio[i].duckSing();
-    // console.log(singer.duckSing())
- }
- for(var i=0;i<cherrio.length;i++){
-     cherrio[i].duckSing();
- }
+Page({
+  data: {
+    logs: []
+  },
+  onLoad: function () {
+    this.setData({
+      logs: (wx.getStorageSync('logs') || []).map(log => {
+        return util.formatTime(new Date(log))
+      })
+    })
+  }
+})
