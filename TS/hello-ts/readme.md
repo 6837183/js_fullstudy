6d64612d86714fe15dafd7ec0138b70ac341da97
6837183 <2501520585@qq.com>
2020-08-20 19:10:57 +0800

    b

---
+++ TS/hello-ts/readme.md
@@ -0,0 +1,8 @@
+1. 安装ts
+2. tsc --init 
+3. 编辑tsconfig.json
+
+
+
+<!-- 类型 -->
+boolean number string void null undefined symbol bigint any 
---
+++ TS/hello-ts/src/index.ts
@@ -0,0 +1,31 @@
+// function greeter(person:string) {
+//     return "hello" + person
+// }
+// // greeter(1)
+// const user = "jwk"
+
+
+// 类型
+// const isLoading: boolean = false
+// isLoading = 123
+
+// let num = 6
+// num = '8'
+
+// let book:string = 'typeScript'
+
+// function warnUser():void {
+
+// }
+// const a: void = undefined
+// let aa = undefined
+
+// let sym1 = Symbol('key1')
+// let sym2 = Symbol('key1')
+
+//BigInt 
+const max = BigInt(Number.MAX_SAFE_INTEGER)
+
+const max1 = max + 1n
+const max2 = max + 2n
+console.log(max1 === max2);
